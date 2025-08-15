d8301359693a41ac1790c927fdc986cdc8b819c7
Tommy Palmer <hi@tommyp.org>
2025-08-15 11:59:01 +0100

    fix retro bins

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,21 +1,27 @@
 // 1. Import utilities from `astro:content`
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z } from 'astro:content';
 
 // 2. Import loader(s)
-import { glob, file } from "astro/loaders";
+import { glob, file } from 'astro/loaders';
 
 // 3. Define your collection(s)
 const bins = defineCollection({
-  loader: file("src/data/bins.json"),
+  loader: file('src/data/bins.json'),
   schema: ({ image }) =>
     z.object({
       fileName: image(),
       slug: z.string(),
-      localAuthorityCountry: z.string(),
-      localAuthorityCode: z.string(),
-      collectionDate: z.coerce.date(),
-      colour: z.string(),
-      councilName: z.string(),
+      localAuthorityCountry: z.optional(z.string()),
+      localAuthorityCode: z.optional(z.string()),
+      collectionDate: z.optional(z.coerce.date()),
+      colour: z.optional(z.string()),
+      councilName: z.optional(z.string()),
+      retro: z.optional(z.boolean()),
+      retroWhy: z.optional(z.string()),
+      retroName: z.optional(z.string()),
+      presentTwinFileName: z.optional(image()),
+      startDate: z.optional(z.string()),
+      endDate: z.optional(z.string()),
     }),
 });
 
